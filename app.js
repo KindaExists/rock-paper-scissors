d221ced900b746e2237a0292245c8ec054820351
KindaExists <ehrencastillo@gmail.com>
2021-07-30 14:10:48 +0800

    created initial functions

---
+++ app.js
@@ -0,0 +1,14 @@
+
+function playRound(playerSelection, computerSelection) {
+    // Runs a single round of the game, playerSelection is case-insensitive
+
+}
+
+function computerPlay() {
+    // Will randomly return either 'rock', 'paper', or 'scissors'
+
+}
+
+const playerSelection = "rock"
+const computerSelection = computerPlay()
+console.log(playRound(playerSelection, computerSelection))
